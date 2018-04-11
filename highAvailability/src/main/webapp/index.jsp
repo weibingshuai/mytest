9f49362d4b83b17af4164dbc62852eb1e3ffc53a
weibs <15829916606@139.com>
2018-04-11 15:46:04 +0800

    fdxfdf

---
+++ highAvailability/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>this is tomcat 3!</h1>
+
+</body>
+</html>
---
+++ test/src/main/java/testDemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Create by weibs on 2018/4/8 17:49
+ */
+public class testDemo {
+    public static void main(String[]args){
+        System.out.println("这是主函数");
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("zhangsan");
+        list.add("list");
+        int a ='A'/'a'>1?4:5;
+        System.out.println(a+='A');
+    }
+}
